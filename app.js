d04497d4d0a0111190c265dfd6ce1761f5ec4aad
Shaneel Kumar <shaneel_kumar@live.com>
2022-03-16 21:31:15 +1300

    Finished UI and added decimal place functionaility.

---
+++ app.js
@@ -0,0 +1,29 @@
+buttons = document.querySelectorAll('.num-button');
+display = document.querySelector('.display');
+
+// Hover styles for all buttons.
+buttons.forEach(element => element.addEventListener('mouseenter', () => {
+    element.classList.add('hover');
+}));
+buttons.forEach(element => element.addEventListener('mouseleave', () => {
+    element.classList.remove('hover');
+}));
+buttons.forEach(element => element.addEventListener('mousedown', () => {
+    element.classList.add('click');
+}));
+buttons.forEach(element => element.addEventListener('mouseup', () => {
+    element.classList.remove('click');
+}));
+
+function displayInput(buttonInput) {
+    const content = display.textContent;
+    console.log(typeof content);
+    if (buttonInput == '.' && content.includes('.')) return
+    display.textContent += `${buttonInput}`;
+}
+
+buttons.forEach(element => element.addEventListener('click', () => {
+    if (element.classList.contains('num') || element.classList.contains('symbol')) {
+        displayInput(element.innerHTML);
+    }
+}));
